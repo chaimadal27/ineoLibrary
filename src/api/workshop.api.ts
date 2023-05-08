e949491d0ab766476f2d85c32c24dc444d3e0985
chaiD12 <daldoulchaima90@gmail.com>
2023-05-08 01:30:16 +0100

    change fetch

--- src/api/workshop.api.ts
+++ src/api/workshop.api.ts
@@ -5,22 +5,22 @@ import { Workshop } from "@app/store/slices/workshopSlice";
 
 
 export const addWorkshop = async(workshop:Workshop):Promise<Workshop> => 
-  httpApi.post<Workshop>('http://localhost:8000/workshop/create/lanes/',{...workshop})
+  httpApi.post<Workshop>('http://162.19.153.94:8000/workshop/create/lanes/',{...workshop})
     .then(({data})=>data)
 
 export const getWorkshops = async():Promise<Workshop[]> => 
-    httpApi.get<Workshop[]>('http://localhost:8000/workshop/list/')
+    httpApi.get<Workshop[]>('http://162.19.153.94:8000/workshop/list/')
     .then(({data})=>data)
 
 export const updateWorkshop = async(workshop:Workshop):Promise<Workshop> => 
-    httpApi.patch<Workshop>(`http://localhost:8000/workshop/${workshop.id}/update/lanes/`,{...workshop})
+    httpApi.patch<Workshop>(`http://162.19.153.94:8000/workshop/${workshop.id}/update/lanes/`,{...workshop})
     .then(({data})=>data)
 
 export const updateWorkshopImage = async(workshop:Workshop):Promise<Workshop> => 
-    httpApi.patch<Workshop>(`http://localhost:8000/workshop/${workshop.id}/update/image/`,{...workshop}, {headers:{'Content-Type':'multipart/form-data; boundary=----------'}})
+    httpApi.patch<Workshop>(`http://162.19.153.94:8000/workshop/${workshop.id}/update/image/`,{...workshop}, {headers:{'Content-Type':'multipart/form-data; boundary=----------'}})
     .then(({data})=>data)
 
 export const getSingleWorkshop = async(workshop:Workshop):Promise<Workshop> =>
-    httpApi.get<Workshop>(`http://localhost:8000/workshop/${workshop.id}/details/`)
+    httpApi.get<Workshop>(`http://162.19.153.94:8000/workshop/${workshop.id}/details/`)
     .then(({data})=>data)
 
